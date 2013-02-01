895e2fad4bd7c9525f7239bf2b9bdbbc47f16a20
Nipper Phinizy <nipper@gmail.com>
2013-01-31 21:20:22 -0500

    no clue

--- goblin.py
+++ goblin.py
@@ -4,20 +4,19 @@ from model import *
 
 def updateFeedEntries(feed):
 
-    parsedFeed = feedparser.parse(feed.url)
-    
-    for entry in parsedFeed.entries:
-        newLink,newTitle = entry.link,entry.title
-        try:
-            newEntry = Entries.get(Entries.url == entry.link)
-        except Entries.DoesNotExist:
-            newEntry = Entries(entryTitle=newTitle,url=newLink,feed=feed)
-            newEntry.save()
+  parsedFeed = feedparser.parse(feed.url)
+
+  for entry in parsedFeed.entries:
+      newLink,newTitle = entry.link,entry.title
+      try:
+        newEntry = Entries.get(Entries.url == entry.link)
+      except Entries.DoesNotExist:
+        newEntry = Entries(entryTitle=newTitle,url=newLink,feed=feed)
+        newEntry.save()
 
 def updateFeeds():
 
-    for feed in Feeds.select():
-        print(feed.title)
-
-        updateFeedEntries(feed)
+  for feed in Feeds.select():
+    print(feed.title)
+    updateFeedEntries(feed)
 
